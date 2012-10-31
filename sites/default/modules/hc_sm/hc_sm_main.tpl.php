ec0bd03b4b2a8949076a6008b93a5b9f1d5437c3
Gerrit Vos <gevo27@gmail.com>
2012-10-31 12:23:12 +0100

    Social Media

---
+++ sites/default/modules/hc_sm/hc_sm_main.tpl.php
@@ -0,0 +1,27 @@
+<!-- Facebook Like Button BEGIN -->
+<div class="facebook">
+<iframe src="//www.facebook.com/plugins/like.php?href=<?php print $path; ?>&amp;send=false&amp;layout=button_count&amp;width=130&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=21&amp;appId=488944954471810" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:130px; height:21px;" allowTransparency="true"></iframe>
+</div>
+<!-- Facebook Like Button END -->
+
+<!-- Tweet Button BEGIN -->
+<div class="twitter">
+<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
+<!-- Tweet Button END -->
+
+<!-- Google+ Button BEGIN -->
+<div class="google">
+<g:plusone size="medium" annotation="inline" width="150"></g:plusone>
+<script type="text/javascript">
+  window.___gcfg = {lang: 'en-GB'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+</div>
+<!-- Google+ Button END -->

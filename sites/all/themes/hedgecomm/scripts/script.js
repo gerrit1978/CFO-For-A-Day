619f736ba74b95b197f440b8990ac73eebf2a97c
Gerrit Vos <gevo27@gmail.com>
2012-11-10 09:30:44 +0100

    Slicing

--- sites/all/themes/hedgecomm/scripts/script.js
+++ sites/all/themes/hedgecomm/scripts/script.js
@@ -4,5 +4,15 @@ jQuery(document).ready(function($) {
 	$('#countdown').countdown({ 
     until: new Date(2013, 4 - 1, 21)
   });
+  
+  // wildcard slideDown: adjust animation top to CSS height
+  $('#block-block-1 .wildcard').mouseover(function() {
+		$(this).stop().animate({top: '350px'}, "slow");
+  });
+  $('#block-block-1 .wildcard').mouseout(function() {
+		$(this).stop().animate({top: '150px'}, "slow");
+  });
+
+
 
 });
